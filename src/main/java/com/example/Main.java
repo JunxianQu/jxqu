829a5f5f0eb342711505e0cbf351bbef888dd507
JunxianQu <isjunxianqu@gmail.com>
2021-07-02 15:16:40 -0700

    tls to ssl

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -66,18 +66,18 @@ public class Main {
       String visitorMail = request.getParameter("e-mail");
       String visitorMessage = request.getParameter("message");
       
-      // final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+      final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
  // Get a Properties object
     // Properties props = System.getProperties();
     Properties props = new Properties();
     props.setProperty("mail.smtp.host", "smtp.gmail.com");
-    // props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+    props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
     // props.setProperty("mail.smtp.socketFactory.fallback", "false");
     props.setProperty("mail.smtp.port", "587");
-    // props.setProperty("mail.smtp.socketFactory.port", "465");
+    props.setProperty("mail.smtp.socketFactory.port", "465");
     props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.starttls.enable", "true");
-    // props.put("mail.smtp.ssl.enable", "true");
+    // props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.smtp.ssl.enable", "true");
     // props.put("mail.debug", "true");
     // props.put("mail.store.protocol", "pop3");
     // props.put("mail.transport.protocol", "smtp");
